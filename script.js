bf0f8ce4d342acd49faeea9e164d040a84eb57b4
hcinfo9 <hcinformatica9438@gmail.com>
2024-06-10 06:37:01 -0300

    Ajuste para expamar comando de sinais

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const limpaTela = document.querySelector('.limpa-tela')
 const valorDisplay = document.querySelector('.display')
+const restrictedSymbols = [".", "+", "/", "*", "-"];
+const operadores = document.querySelectorAll('.operator')
+const ponto = document.querySelector('.ponto')
 
 // valida se alguma letra ou caractere não permitido é digitado e apaga ele
 valorDisplay.addEventListener('keyup', (e) => {
@@ -7,6 +10,32 @@ valorDisplay.addEventListener('keyup', (e) => {
     e.target.value = newValue
 
 })
+
+  valorDisplay.addEventListener('keydown', (event) => {
+    if (restrictedSymbols.includes(event.key)) {
+        const lastChar = valorDisplay.value.slice(-1);
+        if (restrictedSymbols.includes(lastChar)) {
+            event.preventDefault(); // Prevent duplicate symbol
+        }
+    }
+  });
+
+ponto.addEventListener('click', () => ponto.disabled=valorDisplay.value.includes('.'))
+
+
+operadores.forEach((operador) => {
+    operador.addEventListener('click', ()=>{
+  
+        if(valorDisplay.value) {
+            operadores.forEach((element) =>{
+                element.disabled=true 
+                ponto.disabled=false
+            })
+        }
+    })
+})
+
+   
 
 const adicionaValores = (number) => {
     valorDisplay.value += number
@@ -16,14 +45,22 @@ const resultadoConta = () => {
     if(valorDisplay.value.includes('/0')){
         valorDisplay.style.fontSize='28px'
         valorDisplay.value = 'Não divisivél por zero'
-        display = setTimeout(() =>{
+        setTimeout(() =>{
             valorDisplay.style.fontSize='48px'
             valorDisplay.value=''
         },2000)
     }
     else {
-        valorDisplay.value = eval(valorDisplay.value)
+        try{
+            valorDisplay.value = eval(valorDisplay.value)
+        }catch (error){
+            console.error("Error:", error.message)
+            valorDisplay.value='Valores invalidos'
+        }
+        
     }
+    operadores.forEach((element) => element.disabled=false)
+    ponto.disabled=false
     
 }
 
@@ -37,5 +74,9 @@ limpaTela.addEventListener('click', () => {
     if (valorDisplay.value === "") return
 
     valorDisplay.value = ""
+
+    operadores.forEach((element) => element.disabled=false)
+    ponto.disabled=false
 })
+
 
